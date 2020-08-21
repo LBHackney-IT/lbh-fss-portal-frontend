eaec5497c7037b2b20c580b800e9138102399c3e
Daniel <daniel@Daniels-MacBook-Pro.local>
2020-08-21 17:43:07 +0100

    added tests for updated ProtectedLayout component

--- src/components/ProtectedLayout/ProtectedLayout.test.js
+++ src/components/ProtectedLayout/ProtectedLayout.test.js
@@ -1,1 +1,59 @@
-//TODO:
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import ProtectedLayout from './ProtectedLayout';
+import UserContext from '../../context/UserContext/UserContext';
+
+
+let fakeUser = {
+    id: 1,
+    name: 'fakeName',
+    email: 'fakeEmail@example.com',
+    organisation: { name: 'fakeOrganisation' },
+    statuses: 'active'
+}
+
+function renderWithPermissions() {
+    fakeUser.roles = ['hackney_admin', 'hackney_viewer']
+    const fakeUserWithPermissions = fakeUser
+    render(
+        <UserContext.Provider value={[fakeUserWithPermissions, null]}>
+            <ProtectedLayout />
+        </UserContext.Provider>
+    );
+}
+
+function renderWithoutPermissions() {
+    fakeUser.roles = ['vsco_contributer']
+    const fakeUserWithoutPermissions = fakeUser
+    render(
+        <UserContext.Provider value={[fakeUserWithoutPermissions, null]}>
+            <ProtectedLayout />
+        </UserContext.Provider>
+    );
+}
+
+// 
+test('"users" menu link does appear for users with permissions', async () => {
+    renderWithPermissions()
+    const menuLinks = await screen.findByRole('navigation')
+    expect(menuLinks).toHaveTextContent('Users')
+})
+
+test('"analytics" menu link does appear for users with permissions', async () => {
+    renderWithPermissions()
+    const menuLinks = await screen.findByRole('navigation')
+    expect(menuLinks).toHaveTextContent('Analytics')
+})
+
+test('"users" menu link does not appear for users without permissions', async () => {
+    renderWithoutPermissions()
+    const menuLinks = await screen.findByRole('navigation')
+    expect(menuLinks).not.toHaveTextContent('Users')
+
+})
+
+test('"analytics" menu link does not appear for users without permissions', async () => {
+    renderWithoutPermissions()
+    const menuLinks = await screen.findByRole('navigation')
+    expect(menuLinks).not.toHaveTextContent('Analytics')
+})
--- src/domain/Users/UserTable/UserTable.test.js
+++ src/domain/Users/UserTable/UserTable.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, waitFor, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import axiosMock from 'axios';
 import UserTable from './UserTable';
 
