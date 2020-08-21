11a14d972fa0c468576270259ecf0c6065c394fc
Daniel <daniel@Daniels-MacBook-Pro.local>
2020-08-21 11:41:14 +0100

    updated use of functions in login.test file - tests are the same but use of functions are different

--- src/domain/Authentication/Login/Login.test.js
+++ src/domain/Authentication/Login/Login.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, waitFor } from '@testing-library/react';
+import { render, fireEvent, waitFor, screen } from '@testing-library/react';
 import Login from './Login';
 import axiosMock from 'axios';
 import { repeat } from 'lodash';
@@ -21,152 +21,151 @@ test('email and password fields present', async () => {
 });
 
 test('empty fields trigger validation message', async () => {
-    const { getByTestId, getByText } = render(<Login />);
+    render(<Login />);
 
-    fireEvent.submit(getByTestId('form'));
+    fireEvent.submit(screen.getByTestId('form'));
 
-    await waitFor(() => {
-        expect(getByText(/email is required/i)).toBeInTheDocument()
-    })
-    await waitFor(() => {
-        expect(getByText(/password is required/i)).toBeInTheDocument()
-    })
+    const emailAlert = await screen.findByText(/email is required/i)
+    const passwordAlert = await screen.findByText(/password is required/i)
+
+    expect(emailAlert).toBeInTheDocument()
+    expect(passwordAlert).toBeInTheDocument()
 });
 
 test('email maxLength triggers validation', async () => {
-    const { getByTestId, getByLabelText, getByText } = render(<Login />);
+    render(<Login />);
 
     const emailLong = repeat(mockUser.email, 100)
 
-    fireEvent.change(getByLabelText(/email/i), { target: { value: emailLong } });
+    fireEvent.change(screen.getByLabelText(/email/i), { target: { value: emailLong } });
 
-    fireEvent.submit(getByTestId('form'));
+    fireEvent.submit(screen.getByTestId('form'));
 
-    await waitFor(() => {
-        expect(getByText(/Max length exceeded/i)).toBeInTheDocument()
-    })
+    const emailAlert = await screen.findByText(/Max length exceeded/i)
+
+    expect(emailAlert).toBeInTheDocument()
 });
 
 test('emails with incorrect pattern trigger validation', async () => {
-    const { getByTestId, getByLabelText, getByText } = render(<Login />);
+    render(<Login />);
 
     const emailsWithIncorrectPattern = ['test@.com', 'test', 'a@b', 'abc@com']
 
     emailsWithIncorrectPattern.forEach(async (email) => {
-        fireEvent.change(getByLabelText(/email/i), { target: { value: email } });
+        fireEvent.change(screen.getByLabelText(/email/i), { target: { value: email } });
 
-        fireEvent.submit(getByTestId('form'));
+        fireEvent.submit(screen.getByTestId('form'));
 
-        await waitFor(() => {
-            expect(getByText(/Enter a valid e-mail address/i)).toBeInTheDocument()
-        })
+        const emailAlert = await screen.findByText(/Enter a valid e-mail address/i)
+
+        expect(emailAlert).toBeInTheDocument()
     })
 });
 
 test('emails with correct pattern do not trigger validation', async () => {
-    const { getByTestId, getByLabelText, getByText } = render(<Login />);
+    render(<Login />);
 
     const emailsWithIncorrectPattern = ['test@example.com', 'test12D@gmail.com', 'nudge@nudgedigital.co.uk', 'microsoft@hotmail.com']
 
     emailsWithIncorrectPattern.forEach(async (email) => {
-        fireEvent.change(getByLabelText(/email/i), { target: { value: email } });
+        fireEvent.change(screen.getByLabelText(/email/i), { target: { value: email } });
 
-        fireEvent.submit(getByTestId('form'));
+        fireEvent.submit(screen.getByTestId('form'));
 
-        await waitFor(() => {
-            expect(getByText(/Enter a valid e-mail address/i)).not.toBeInTheDocument()
-        })
+        const emailAlert = await screen.findByText(/Enter a valid e-mail address/i)
+
+        expect(emailAlert).not.toBeInTheDocument()
     })
 });
 
 test('password maxLength triggers validation', async () => {
-    const { getByTestId, getByLabelText, getByText } = render(<Login />);
+    render(<Login />);
 
     const passwordLong = repeat('Password123', 100)
 
-    fireEvent.change(getByLabelText(/password/i), { target: { value: passwordLong } });
+    fireEvent.change(screen.getByLabelText(/password/i), { target: { value: passwordLong } });
 
-    fireEvent.submit(getByTestId('form'));
+    fireEvent.submit(screen.getByTestId('form'));
 
-    await waitFor(() => {
-        expect(getByText(/Max length exceeded/i)).toBeInTheDocument()
-    })
+    const passwordAlert = await screen.findByText(/Max length exceeded/i)
+
+    expect(passwordAlert).toBeInTheDocument()
 });
 
 test('password minLength triggers validation', async () => {
-    const { getByTestId, getByLabelText, getByText } = render(<Login />);
+    render(<Login />);
 
     const passwordShort = 'Pass1'
 
-    fireEvent.change(getByLabelText(/password/i), { target: { value: passwordShort } });
+    fireEvent.change(screen.getByLabelText(/password/i), { target: { value: passwordShort } });
 
-    fireEvent.submit(getByTestId('form'));
+    fireEvent.submit(screen.getByTestId('form'));
 
-    await waitFor(() => {
-        expect(getByText(/Password must be more than 6 characters/i)).toBeInTheDocument()
-    })
+    const passwordAlert = await screen.findByText(/Password must be at least 6 characters/i)
+
+    expect(passwordAlert).toBeInTheDocument()
 });
 
 test('passwords missing at least one capital letter trigger validation', async () => {
-    const { getByTestId, getByLabelText, getByText } = render(<Login />);
+    render(<Login />);
 
     const passwordsWithoutCapital = ['password123', 'aaa_111', 'AbCdEfG1']
 
     passwordsWithoutCapital.forEach(async (password) => {
-        fireEvent.change(getByLabelText(/password/i), { target: { value: password } });
+        fireEvent.change(screen.getByLabelText(/password/i), { target: { value: password } });
 
-        fireEvent.submit(getByTestId('form'));
+        fireEvent.submit(screen.getByTestId('form'));
 
-        await waitFor(() => {
-            expect(getByText(/Password must contain at least one capital letter/i)).toBeInTheDocument()
-        })
+        const passwordAlert = await screen.findByText(/Password must contain at least one capital letter/i)
+
+        expect(passwordAlert).toBeInTheDocument()
     })
 });
 
 test('passwords missing at least one number trigger validation', async () => {
-    const { getByTestId, getByLabelText, getByText } = render(<Login />);
+    render(<Login />);
 
     const passwordsWithoutNumber = ['Password', 'aaa_AAA', 'aAbBcCdDeE']
 
     passwordsWithoutNumber.forEach(async (password) => {
-        fireEvent.change(getByLabelText(/password/i), { target: { value: password } });
+        fireEvent.change(screen.getByLabelText(/password/i), { target: { value: password } });
 
-        fireEvent.submit(getByTestId('form'));
+        fireEvent.submit(screen.getByTestId('form'));
 
-        await waitFor(() => {
-            expect(getByText(/Password must contain at least one number/i)).toBeInTheDocument()
-        })
+        const passwordAlert = await screen.findByText(/Password must contain at least one number/i)
+
+        expect(passwordAlert).toBeInTheDocument()
     })
 });
 
 test('passwords with correct pattern do not trigger validation', async () => {
-    const { getByTestId, getByLabelText, getByText } = render(<Login />);
+    render(<Login />);
 
     const passwordsWithIncorrectPattern = ['xfgn@D4d', 'wbfye6dvcEEd', '1fnfrf][D6d/', 'pedk8~d??wQWE']
 
     passwordsWithIncorrectPattern.forEach(async (passwords) => {
-        fireEvent.change(getByLabelText(/password/i), { target: { value: passwords } });
+        fireEvent.change(screen.getByLabelText(/password/i), { target: { value: passwords } });
 
-        fireEvent.submit(getByTestId('form'));
+        fireEvent.submit(screen.getByTestId('form'));
 
-        await waitFor(() => {
-            expect(getByText(/Password must/i)).not.toBeInTheDocument()
-        })
+        const passwordAlert = await screen.findByText(/Password must/i)
+
+        expect(passwordAlert).not.toBeInTheDocument()
     })
 });
 
 test('login as an existing user', async () => {
-    const { getByTestId, getByLabelText } = render(<Login />);
+    render(<Login />);
 
     // update values in form 
-    fireEvent.change(getByLabelText(/email/i), { target: { value: mockUser.email } });
-    fireEvent.change(getByLabelText(/password/i), { target: { value: 'fakePassword123' } });
+    fireEvent.change(screen.getByLabelText(/email/i), { target: { value: mockUser.email } });
+    fireEvent.change(screen.getByLabelText(/password/i), { target: { value: 'fakePassword123' } });
 
     // specify mocked data to be returned
     axiosMock.post.mockImplementationOnce(() => Promise.resolve({ data: mockUser }));
 
     // submit form 
-    fireEvent.submit(getByTestId('form'));
+    fireEvent.submit(screen.getByTestId('form'));
 
     // await redirect from /login to /services 
     await waitFor(() => {
