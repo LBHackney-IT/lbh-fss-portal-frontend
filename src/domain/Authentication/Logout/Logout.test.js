91fa22ae24ed9b8eba3a27f494dbc64594964d2a
Daniel <daniel@Daniels-MacBook-Pro.local>
2020-08-21 11:51:28 +0100

    added test for logout component

--- src/domain/Authentication/Logout/Logout.test.js
+++ src/domain/Authentication/Logout/Logout.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, fireEvent, waitFor } from '@testing-library/react';
+import { render, fireEvent, waitFor, screen } from '@testing-library/react';
 import axiosMock from 'axios';
 import App from '../../../App'
 var mockUsers = require("../../../../mock-api/users/mockUsers.json");
@@ -13,25 +13,23 @@ beforeEach(() => {
 const mockUser = sample(mockUsers);
 
 test('Logout redirects to Login page', async () => {
-    // mock user data 
+    // mock user data (so as to authenticate)
     axiosMock.get.mockImplementationOnce(() => Promise.resolve({ data: mockUser }));
 
     // render App
-    const { getByText, queryByRole, findByText } = render(<App />)
+    render(<App />)
 
     // wait for log out button to render 
-    await findByText(/log out/i)
+    await screen.findByText(/log out/i)
 
     // click log out button 
-    fireEvent.click(getByText(/log out/i));
+    fireEvent.click(screen.getByText(/log out/i));
 
     // check user is directed to /logout page
     expect(window.location.pathname).toEqual('/logout');
 
-
     // check user is redirected from logout page to login page
-    await waitFor(() => {
-        expect(queryByRole('heading', { name: /login/i })).not.toBeNull()
-    })
+    const loginHeader = await screen.findByRole('heading', { name: /login/i })
+    expect(loginHeader).not.toBeNull()
     expect(window.location.pathname).toEqual('/');
 })
