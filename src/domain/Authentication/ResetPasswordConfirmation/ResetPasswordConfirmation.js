cde51334e9d8c4dc7698ed52d34c6176bbe18738
Daniel-NudgeDigital <69238887+Daniel-NudgeDigital@users.noreply.github.com>
2020-09-09 14:25:51 +0100

    feature/reset-password (#11)
    * added password reset form and password reset confirmation form - both hooked up to mock-api
    
    * minor syntax changes
    
    * redirect to login, rather than /services, on successful password change
    
    * remove space under submit buttons
    
    Co-authored-by: Daniel <daniel@Daniels-MacBook-Pro.local>

---
+++ src/domain/Authentication/ResetPasswordConfirmation/ResetPasswordConfirmation.js
@@ -0,0 +1,135 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import styled from "styled-components";
+import FormInput from "../../../components/FormInput/FormInput";
+import Button from "../../../components/Button/Button";
+import AuthenticationService from "../../../services/AuthenticationService/AuthenticationService";
+import { toast } from "react-toastify";
+import { navigate } from "@reach/router";
+import Cookies from "js-cookie";
+
+const StyledButton = styled(Button)`
+  width: 100%;
+  margin: 0;
+`;
+
+const ResetPasswordConfirmation = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  let defaultValues = {
+    email: "",
+    code: "",
+    password: "",
+  };
+
+  if (Cookies.get("passwordReset")) {
+    try {
+      const cookie = JSON.parse(Cookies.get("passwordReset"));
+      defaultValues.email = cookie.email;
+    } catch (err) {
+      Cookies.remove("passwordReset");
+    }
+  }
+
+  const { register, handleSubmit, getValues, errors, reset } = useForm({
+    defaultValues: defaultValues,
+  });
+
+  async function doPasswordResetConfirmation({ email, code, password }) {
+    if (isLoading) return;
+
+    setIsLoading(true);
+
+    const user = await AuthenticationService.passwordRecoveryConfirmation(
+      email,
+      code,
+      password
+    );
+
+    setIsLoading(false);
+
+    if (user) {
+      toast.success("New password successfully set.");
+
+      navigate("/");
+    } else {
+      reset();
+
+      toast.error("Password reset failed.");
+    }
+  }
+
+  return (
+    <>
+      <h1>Password reset confirmation</h1>
+
+      <form onSubmit={handleSubmit(doPasswordResetConfirmation)}>
+        <FormInput
+          label="Email"
+          name="email"
+          register={register}
+          required
+          validate={{
+            pattern: (value) => {
+              return (
+                value.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i) ||
+                "Enter a valid e-mail address"
+              );
+            },
+          }}
+          maxLength={255}
+          error={errors.email}
+        />
+        <FormInput
+          label="Code"
+          name="code"
+          register={register}
+          required
+          maxLength={255}
+          error={errors.code}
+        />
+        <FormInput
+          type="password"
+          label="New password"
+          name="password"
+          register={register}
+          required
+          maxLength={255}
+          minLength={6}
+          validate={{
+            oneCapital: (value) => {
+              return (
+                value.match(/[[A-Z]/) ||
+                "Password must contain at least one capital letter"
+              );
+            },
+            oneNumber: (value) => {
+              return (
+                value.match(/[[0-9]/) ||
+                "Password must contain at least one number"
+              );
+            },
+          }}
+          error={errors.password}
+        />
+        <FormInput
+          type="password"
+          label="Confirm new password"
+          name="confirmPassword"
+          register={register}
+          validate={{
+            passwordMatch: (value) => {
+              return (
+                value === getValues().password || "Passwords should match."
+              );
+            },
+          }}
+          error={errors.confirmPassword}
+        />
+        <StyledButton type="submit" label="Submit" />
+      </form>
+    </>
+  );
+};
+
+export default ResetPasswordConfirmation;
