e3fccc424b42d31f4ab138f441648203da5015a2
DanielGardiner <gardiner.daniel@hotmail.co.uk>
2020-09-28 15:46:58 +0100

    improve auto zoom/center functionality on map

--- src/domain/Services/ServiceForm/Map/Map.js
+++ src/domain/Services/ServiceForm/Map/Map.js
@@ -17,42 +17,22 @@ export default ({ mapStyle, data }) => {
 
   const cleanData = removeEmptyObjFromArrayObj(data);
 
-  const avgLatitude =
-    cleanData.reduce((total, next) => total + parseFloat(next.latitude), 0) /
-    cleanData.length;
-
-  const avgLongitude =
-    cleanData.reduce((total, next) => total + parseFloat(next.longitude), 0) /
-    cleanData.length;
-
   const mapRef = useRef(null);
   const groupRef = useRef(null);
 
-  // TODO: autozoom/center works if using the click button but not so well within the useEffect
-  // note - i want it to work in the useEffect
-  function handleClick() {
-    const map = mapRef.current.leafletElement;
-    const group = groupRef.current.leafletElement;
-    map.fitBounds(group.getBounds());
-  }
-
   useEffect(() => {
-    // console.log("cleanData");
-    // console.log(cleanData);
-    // console.log(cleanData.length === 1);
     if (cleanData.length === 1) return;
 
     const map = mapRef.current.leafletElement;
     const group = groupRef.current.leafletElement;
+
     map.fitBounds(group.getBounds());
   }, [cleanData]);
 
   return (
     <>
-      <button onClick={handleClick}>Zoom</button>
       <Map
-        center={[avgLatitude, avgLongitude]}
-        // center={[1, 2]}
+        center={[cleanData[0].latitude, cleanData[0].longitude]}
         zoom={13}
         style={mapStyle}
         ref={mapRef}
@@ -70,11 +50,7 @@ export default ({ mapStyle, data }) => {
                   parseFloat(address.longitude),
                 ]}
                 key={index}
-              >
-                <Popup>
-                  A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-              </Marker>
+              ></Marker>
             );
           })}
         </FeatureGroup>
