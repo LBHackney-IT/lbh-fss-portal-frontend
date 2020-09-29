742550ef81756baa7197b2c6abfbcba83dfd8bd3
DanielGardiner <gardiner.daniel@hotmail.co.uk>
2020-09-30 00:03:11 +0100

    updated api responses to reflect updated API specs

--- mock-api/account/GET.js
+++ mock-api/account/GET.js
@@ -7,25 +7,25 @@ var sample = require("lodash/sample");
 //     .json(sample(mockUsers.filter((u) => u.name === "Melody Zieme")));
 // };
 
-module.exports = (req, res) => {
-  res
-    .status(200)
-    .json(sample(mockUsers.filter((u) => u.name === "Melody Zieme")));
-};
+// module.exports = (req, res) => {
+//   res
+//     .status(200)
+//     .json(sample(mockUsers.filter((u) => u.name === "Melody Zieme")));
+// };
 
 // module.exports = (req, res) => {
 //   res
 //     .status(200)
 //     .json(sample(mockUsers.filter((u) => u.name === "Tommie Dietrich")));
 // };
-// module.exports = (req, res) => {
-//   res
-//     .status(200)
-//     .json(
-//       sample(
-//         mockUsers.filter(
-//           (u) => u.roles.length === 1 && u.roles.includes("admin")
-//         )
-//       )
-//     );
-// };
+module.exports = (req, res) => {
+  res
+    .status(200)
+    .json(
+      sample(
+        mockUsers.filter(
+          (u) => u.roles.length === 1 && u.roles.includes("admin")
+        )
+      )
+    );
+};
