3677d3a0cdc3175216a7fbedae5cff953757c41c
DanielGardiner <gardiner.daniel@hotmail.co.uk>
2020-09-30 22:33:18 +0100

    added functionality to prepopulate category form page with data

--- src/domain/Services/EditService/EditService.js
+++ src/domain/Services/EditService/EditService.js
@@ -1,15 +1,136 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ServiceForm from "../ServiceForm/ServiceForm";
 import useServiceFetch from "../../../hooks/useServiceFetch/useServiceFetch";
 import ServiceService from "../../../services/ServiceService/ServiceService";
 import { navigate } from "@reach/router";
 import { toast } from "react-toastify";
+import { serviceCategoryCheckboxOptions } from "../../../utils/data/data";
+
+function doFormatDefaultValuesCategories(values) {
+  const categoryIdArray = values.categories.map((category) => {
+    return category.id;
+  });
+
+  let newValues = values;
+
+  serviceCategoryCheckboxOptions.forEach((item) => {
+    if (categoryIdArray.includes(item.value)) {
+      const categoryInfo = values.categories.filter((category) => {
+        return category.id === item.value;
+      });
+
+      newValues[item.id] = true;
+      newValues[item.id.concat("Details")] = categoryInfo[0].description;
+    }
+  });
+
+  delete newValues.categories;
+
+  return newValues;
+}
+
+function doFormatDefaultValuesLocations(values) {
+  let newValues = values;
+  console.log(values.locations);
+
+  return newValues;
+}
+
+function doCleanDefaultValues(values) {
+  let cleanDefaultValues = {};
+  cleanDefaultValues = doFormatDefaultValuesCategories(values);
+  cleanDefaultValues = doFormatDefaultValuesLocations(cleanDefaultValues);
+
+  return cleanDefaultValues;
+}
+
+function doHandleHiddenFieldVisibility(
+  cleanDefaultValues,
+  showHiddenField,
+  setShowHiddenField
+) {
+  if (cleanDefaultValues.lonOrIs) {
+    showHiddenField.lonOrIsDetails = true;
+  }
+
+  if (cleanDefaultValues.anxOrMH) {
+    showHiddenField.anxOrMHDetails = true;
+  }
+
+  if (cleanDefaultValues.safeAndHB) {
+    showHiddenField.safeAndHBDetails = true;
+  }
+
+  if (cleanDefaultValues.exAndWell) {
+    showHiddenField.exAndWellDetails = true;
+  }
+
+  if (cleanDefaultValues.artAndCrtv) {
+    showHiddenField.artAndCrtvDetails = true;
+  }
+
+  if (cleanDefaultValues.foodOrShop) {
+    showHiddenField.foodOrShopDetails = true;
+  }
+  if (cleanDefaultValues.faithAct) {
+    showHiddenField.faithActDetails = true;
+  }
+  if (cleanDefaultValues.monAdv) {
+    showHiddenField.monAdvDetails = true;
+  }
+  if (cleanDefaultValues.emplAdv) {
+    showHiddenField.emplAdvDetails = true;
+  }
+  if (cleanDefaultValues.houseAdv) {
+    showHiddenField.houseAdvDetails = true;
+  }
+  if (cleanDefaultValues.immAdv) {
+    showHiddenField.immAdvDetails = true;
+  }
+
+  setShowHiddenField(showHiddenField);
+}
 
 const EditService = (props) => {
   const { service, isLoading: fetchIsLoading } = useServiceFetch(
     props.serviceId
   );
+
+  const [defaultValues, setDefaultValues] = useState({});
+
   const [submitIsLoading, setSubmitIsLoading] = useState(false);
+
+  const [showHiddenField, setShowHiddenField] = useState({
+    lonOrIsDetails: false,
+    anxOrMHDetails: false,
+    safeAndHBDetails: false,
+    exAndWellDetails: false,
+    artAndCrtvDetails: false,
+    foodOrShopDetails: false,
+    faithActDetails: false,
+    monAdvDetails: false,
+    emplAdvDetails: false,
+    houseAdvDetails: false,
+    immAdvDetails: false,
+  });
+
+  useEffect(() => {
+    if (fetchIsLoading) return;
+
+    const cleanDefaultValues = doCleanDefaultValues(
+      service,
+      showHiddenField,
+      setShowHiddenField
+    );
+
+    doHandleHiddenFieldVisibility(
+      cleanDefaultValues,
+      showHiddenField,
+      setShowHiddenField
+    );
+
+    setDefaultValues(cleanDefaultValues);
+  }, [service, fetchIsLoading, setDefaultValues]);
 
   async function doEditService(formValues) {
     if (submitIsLoading) return;
@@ -32,8 +153,8 @@ const EditService = (props) => {
     }
   }
 
-  if (fetchIsLoading) {
-    return <div data-testid="loading">Loading</div>;
+  if (fetchIsLoading || Object.keys(defaultValues).length === 0) {
+    return <div data-testid="loading">Loading...</div>;
   }
 
   return (
@@ -42,7 +163,9 @@ const EditService = (props) => {
       <h1>Edit your service listing</h1>
       <ServiceForm
         onFormCompletion={doEditService}
-        service={service}
+        defaultValues={defaultValues}
+        showHiddenField={showHiddenField}
+        setShowHiddenField={setShowHiddenField}
         submitLoading={submitIsLoading}
       />
     </>
--- src/utils/data/data.js
+++ src/utils/data/data.js
@@ -38,17 +38,25 @@ const serviceCategoryFields = [
   "lonOrIs",
   "lonOrIsDetails",
   "anxOrMH",
+  "anxOrMHDetails",
   "safeAndHB",
+  "safeAndHBDetails",
   "exAndWell",
   "exAndWellDetails",
   "artAndCrtv",
+  "artAndCrtvDetails",
   "foodOrShop",
+  "foodOrShopDetails",
   "faithAct",
+  "faithActDetails",
   "monAdv",
+  "monAdvDetails",
   "emplAdv",
+  "emplAdvDetails",
   "houseAdv",
   "houseAdvDetails",
   "immAdv",
+  "immAdvDetails",
 ];
 
 const serviceDemographicsFields = [
@@ -63,9 +71,79 @@ const serviceDemographicsFields = [
   "everyone",
 ];
 
+const serviceCategoryCheckboxOptions = [
+  {
+    id: "lonOrIs",
+    value: 1,
+    label: "Loneliness or isolation",
+    help: "For a friendly chat or advice on everyday living.",
+  },
+  {
+    id: "anxOrMH",
+    value: 2,
+    label: "Anxiety or mental health",
+    help: "For any mental health issues you or your loved ones are facing.",
+  },
+  {
+    id: "safeAndHB",
+    value: 3,
+    label: "Safe and healthy body ",
+    help: "For medical conditions, addictions or safety concerns.",
+  },
+  {
+    id: "exAndWell",
+    value: 4,
+    label: "Exercise and wellbeing",
+    help: "For getting fit and healthy through exercise that works for you.",
+  },
+  {
+    id: "artAndCrtv",
+    value: 5,
+    label: "Arts and creativity ",
+    help: "For classes and education that improve emotional wellbeing. ",
+  },
+  {
+    id: "foodOrShop",
+    value: 6,
+    label: "Food or shopping",
+    help: "For foodbanks, hot food or grocery provision.",
+  },
+  {
+    id: "faithAct",
+    value: 7,
+    label: "Faith-led activities",
+    help: "For activites and groups focussing on religion or spirituality.",
+  },
+  {
+    id: "monAdv",
+    value: 8,
+    label: "Money advice",
+    help: "For information and guiance on debt, benefits and finances.",
+  },
+  {
+    id: "emplAdv",
+    value: 9,
+    label: "Employment advice",
+    help: "For help with finding a job, careers and employment rights.",
+  },
+  {
+    id: "houseAdv",
+    value: 11,
+    label: "Housing advice",
+    help: "For advice on tenancy rights, accommodation and homelessness.",
+  },
+  {
+    id: "immAdv",
+    value: 12,
+    label: "Immigration advice",
+    help: "For help and advice on immigration, asylum and refugee status.",
+  },
+];
+
 export {
   organisationFormFields,
   organisationFormYesNoRadioFields,
   serviceCategoryFields,
   serviceDemographicsFields,
+  serviceCategoryCheckboxOptions,
 };
