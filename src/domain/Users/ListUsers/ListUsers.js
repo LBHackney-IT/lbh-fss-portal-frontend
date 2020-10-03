e7451adfc71d9d5af21855c093fc35c9d2c8c4fc
DanielGardiner <gardiner.daniel@hotmail.co.uk>
2020-10-03 15:29:28 +0100

    started adding ability to download csv of users

--- src/domain/Users/ListUsers/ListUsers.js
+++ src/domain/Users/ListUsers/ListUsers.js
@@ -6,9 +6,10 @@ import Button from "../../../components/Button/Button";
 import UserContext from "../../../context/UserContext/UserContext";
 import AccessDenied from "../../Error/AccessDenied/AccessDenied";
 import UserService from "../../../services/UserService/UserService";
-import { grey } from "../../../settings";
+import { grey, green } from "../../../settings";
 import styled from "styled-components";
 import { breakpoint } from "../../../utils/breakpoint/breakpoint";
+import { CSVLink } from "react-csv";
 
 const StyledActionDiv = styled.div`
   display: flex;
@@ -31,15 +32,39 @@ const StyledButton = styled(Button)`
 `;
 
 const StyledAddUserLink = styled.div`
-  margin-top: 20px;
+  margin: 20px 20px 0 0;
   ${breakpoint("sm")`
-  margin-top: 0;
+  margin: 0 20px 0 0;
   `};
 `;
 
 const StyledLink = styled(Link)`
   text-decoration: none;
 `;
+
+function cleanDataForExport(data) {
+  const newData = data
+    .filter((user) => user.status === "active")
+    .map((user) => {
+      user.organisationName = user.organisation.name;
+
+      if (user.roles.includes("vcso")) {
+        user.rolesVcso = "Yes";
+      }
+
+      if (user.roles.includes("viewer")) {
+        user.rolesViewer = "Yes";
+      }
+
+      if (user.roles.includes("admin")) {
+        user.rolesAdmin = "Yes";
+      }
+
+      return user;
+    });
+  // console.log(newData);
+  return newData;
+}
 
 const ListUsers = ({ location }) => {
   const { roles } = useContext(UserContext)[0];
@@ -71,16 +96,48 @@ const ListUsers = ({ location }) => {
 
   const accessPermission = roles.includes("viewer") || roles.includes("admin");
 
+  const headersForExport = [
+    { label: "ID", key: "id" },
+    { label: "Name", key: "name" },
+    { label: "Email", key: "email" },
+    { label: "Organisation", key: "organisationName" },
+    { label: "VCSO user", key: "rolesVcso" },
+    { label: "Viewer user", key: "rolesViewer" },
+    { label: "Admin user", key: "rolesAdmin" },
+    { label: "Status", key: "status" },
+  ];
+
+  const dataForExport = cleanDataForExport(data);
+
+  console.log("dataForExport");
+  console.log(dataForExport);
+
   return accessPermission ? (
     <>
       <div>
         <StyledActionDiv>
           <Search setSearch={setSearch} />
-          <StyledAddUserLink>
-            <StyledLink to="/users/add">
-              <StyledButton label={"Add user"} />
-            </StyledLink>
-          </StyledAddUserLink>
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <StyledAddUserLink>
+              <StyledLink to="/users/add">
+                <StyledButton label={"Add user"} padding="15px 30px" />
+              </StyledLink>
+            </StyledAddUserLink>
+            <StyledButton
+              label={"Export all users"}
+              padding="15px 25px"
+              backgroundColor="white"
+              color={green[400]}
+              border={`1px solid ${green[400]}`}
+            />
+            <CSVLink
+              data={dataForExport}
+              headers={headersForExport}
+              filename={"portal_users.csv"}
+            >
+              Download me
+            </CSVLink>
+          </div>
         </StyledActionDiv>
       </div>
       <UserTable data={data} isLoading={isLoading} search={search} />
