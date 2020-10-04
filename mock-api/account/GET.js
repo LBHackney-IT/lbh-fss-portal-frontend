109ef64d6d1dffbefddd4dc80b8acb068a640d32
DanielGardiner <gardiner.daniel@hotmail.co.uk>
2020-10-04 21:57:46 +0100

    update starting stage of form

--- mock-api/account/GET.js
+++ mock-api/account/GET.js
@@ -13,11 +13,11 @@ var sample = require("lodash/sample");
 //     );
 // };
 
-// VCSO - NO services but WITH organisation
+// ADMIN
 module.exports = (req, res) => {
   res
     .status(200)
-    .json(sample(mockUsers.filter((u) => u.name === "Melody Zieme")));
+    .json(sample(mockUsers.filter((u) => u.name === "Reyna Simonis")));
 };
 
 // // VCSO - WITH services and WITH organisation
@@ -27,16 +27,16 @@ module.exports = (req, res) => {
 //     .json(sample(mockUsers.filter((u) => u.name === "Tommie Dietrich")));
 // };
 
+// // VCSO - NO services but WITH organisation
+// module.exports = (req, res) => {
+//   res
+//     .status(200)
+//     .json(sample(mockUsers.filter((u) => u.name === "Melody Zieme")));
+// };
+
 // // VCSO - WITH services but WITHOUT organisations
 // module.exports = (req, res) => {
 //   res
 //     .status(200)
 //     .json(sample(mockUsers.filter((u) => u.name === "Jaylin Rutherford Sr.")));
-// };
-
-// // ADMIN
-// module.exports = (req, res) => {
-//   res
-//     .status(200)
-//     .json(sample(mockUsers.filter((u) => u.name === "Reyna Simonis")));
 // };
--- src/domain/Services/ServiceForm/ServiceForm.js
+++ src/domain/Services/ServiceForm/ServiceForm.js
@@ -55,8 +55,7 @@ const ServiceForm = ({
   defaultValues = {},
   showHiddenField,
   setShowHiddenField,
-  initialStepId = "categories",
-  // initialStepId = "details",
+  initialStepId = "details",
   submitLoading = false,
 }) => {
   const stepArray = [
