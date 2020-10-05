6b5d2641ec4594bbbfb261bf89514b8fd6f06487
DanielGardiner <gardiner.daniel@hotmail.co.uk>
2020-10-05 16:40:57 +0100

    updated mock returned data

--- mock-api/session/POST.js
+++ mock-api/session/POST.js
@@ -1,18 +1,14 @@
 var mockUsers = require("../mockUsers.json");
 var sample = require("lodash/sample");
 
-// module.exports = (req, res) => {
-//   const email = req.body.email;
-
-//   const mockUser = sample(mockUsers);
-
-//   mockUser.email = email;
-
-//   res.status(200).json(mockUser);
-// };
-
 module.exports = (req, res) => {
   res
     .status(200)
     .json(sample(mockUsers.filter((u) => u.name === "Tommie Dietrich")));
 };
+
+// module.exports = (req, res) => {
+//   res
+//     .status(200)
+//     .json(sample(mockUsers.filter((u) => u.name === "Melody Zieme")));
+// };
