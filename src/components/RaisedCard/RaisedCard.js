307e9d6e35635503472da2c78b03ca4ce85396bd
DanielGardiner <gardiner.daniel@hotmail.co.uk>
2020-10-07 19:01:27 +0100

    update form navigation menu to revert to accordion on mobile view

--- src/components/RaisedCard/RaisedCard.js
+++ src/components/RaisedCard/RaisedCard.js
@@ -5,12 +5,12 @@ import { breakpoint } from "../../utils/breakpoint/breakpoint";
 
 const StyledDiv = styled.div`
   border-top: 3px solid ${green[400]} !important;
-  padding: ${(props) => props.padding || "20px"};
   border: 2px solid #dee0e2;
   box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.05);
   border-radius: 3px;
   background-color: ${(props) => props.backgroundColor || grey[300]};
   margin: ${(props) => props.margin || "0"};
+  padding: ${(props) => props.padding || "20px"};
   width: ${(props) => props.widthMobile || "100%"};
   ${breakpoint("md")`
    width: ${(props) => props.widthMedium || "100%"};
