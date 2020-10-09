72fbfd8d71db6a0fcca9acac2bf36a26955e532d
DanielGardiner <gardiner.daniel@hotmail.co.uk>
2020-10-09 16:09:33 +0100

    removed baseurl and replaced with /api on api calls

--- src/services/AuthenticationService/AuthenticationService.js
+++ src/services/AuthenticationService/AuthenticationService.js
@@ -1,59 +1,23 @@
 import axios from "axios";
 import API_KEY from "../../settings/apiKey";
-import BASE_API_URL from "../../settings/baseApiUrl";
 
 const AuthenticationService = {
   async register(name, email, password) {
     try {
-      // TODO: Live API
-      // const response = await axios.post(`${BASE_API_URL}/registration`, {
-      //   data: {
-      //     name: name,
-      //     email: email,
-      //     password: password,
-      //   },
-      //   headers: {
-      //     "x-api-key": API_KEY,
-      //     "Content-Type": "application/json",
-      //   },
-      // });
-
-      // // TODO: Live API
       const response = await axios.post(
-        `${BASE_API_URL}/registration`,
+        "api/registration",
+        {
+          name: name,
+          email: email,
+          password: password,
+        },
         {
           headers: {
             "x-api-key": API_KEY,
             "Content-Type": "application/json",
           },
-          params: {
-            now: "NOW",
-          },
-          data: {
-            name: name,
-            email: email,
-            password: password,
-          },
         }
       );
-      // // // TODO: Live API
-      // const response = await axios.post(
-      //   `${BASE_API_URL}/registration`,
-      //   {
-      //     name: name,
-      //     email: email,
-      //     password: password,
-      //   },
-      //   {
-      //     headers: {
-      //       "x-api-key": API_KEY,
-      //       "Content-Type": "application/json",
-      //     },
-      //     params: {
-      //       now: "NOW",
-      //     },
-      //   }
-      // );
 
       return response.data;
     } catch (error) {
@@ -62,9 +26,8 @@ const AuthenticationService = {
   },
   async registerConfirmation(email, code) {
     try {
-      // TODO: Live API
       const response = await axios.post(
-        `${BASE_API_URL}/registration/confirmation`,
+        "api/registration/confirmation",
         {
           email: email,
           code: code,
@@ -84,9 +47,8 @@ const AuthenticationService = {
   },
   async resendRegisterConfirmation(email) {
     try {
-      // TODO: Live API
       await axios.post(
-        `${BASE_API_URL}/registration/confirmation/resend`,
+        "api/registration/confirmation/resend",
         {
           email: email,
         },
@@ -105,9 +67,8 @@ const AuthenticationService = {
   },
   async login(email, password) {
     try {
-      // TODO: Live API
       await axios.post(
-        `${BASE_API_URL}/session`,
+        "api/session",
         { email: email, password: password },
         {
           headers: {
@@ -124,19 +85,13 @@ const AuthenticationService = {
   },
   async me() {
     try {
-      // TODO: Mock API
-      const response = await axios.get("/api/account");
+      const response = await axios.get("api/account", {
+        headers: {
+          "x-api-key": API_KEY,
+        },
+      });
 
       return response.data;
-
-      // TODO: Live API
-      // const response = await axios.get(`${BASE_API_URL}/account`, {
-      //   headers: {
-      //     "x-api-key": API_KEY,
-      //   },
-      // });
-
-      // return response.data;
     } catch (error) {
       return false;
     }
@@ -151,73 +106,45 @@ const AuthenticationService = {
     }
   },
   async passwordRecovery(email) {
-    // TODO: Mock API
     try {
-      await axios.post("api/password-recovery", { email: email });
-      console.log("pass");
+      await axios.post(
+        "api/password-recovery",
+        {
+          email: email,
+        },
+        {
+          headers: {
+            "x-api-key": API_KEY,
+            "Content-Type": "application/json",
+          },
+        }
+      );
       return true;
     } catch (error) {
-      console.log("fail");
       return false;
     }
-
-    // TODO: Live API
-    // try {
-    //   await axios.post(
-    //     `${BASE_API_URL}/password-recovery`,
-    //     {
-    //       email: email,
-    //     },
-    //     {
-    //       headers: {
-    //         "x-api-key": API_KEY,
-    //         "Content-Type": "application/json",
-    //       },
-    //     }
-    //   );
-    //   console.log("pass");
-    //   return true;
-    // } catch (error) {
-    //   console.log("fail");
-    //   return false;
-    // }
   },
   async passwordRecoveryConfirmation(email, code, password) {
-    // TODO: Mock API
     try {
-      await axios.post("api/password-recovery/confirmation", {
-        email: email,
-        code: code,
-        password: password,
-      });
-      console.log("pass");
+      await axios.post(
+        "api/password-recovery/confirmation",
+        {
+          email: email,
+          code: code,
+          password: password,
+        },
+        {
+          headers: {
+            "x-api-key": API_KEY,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
       return true;
     } catch (error) {
-      console.log("fail");
       return false;
     }
-
-    // TODO: Live API
-    // try {
-    //   await axios.post(
-    //     `${BASE_API_URL}/password-recovery/confirmation`,
-    //     {
-    //       email: email,
-    //       code: code,
-    //       password: password,
-    //     },
-    //     {
-    //       headers: {
-    //         "x-api-key": API_KEY,
-    //         "Content-Type": "application/json",
-    //       },
-    //     }
-    //   );
-
-    //   return true;
-    // } catch (error) {
-    //   return false;
-    // }
   },
 };
 
--- src/services/UserService/UserService.js
+++ src/services/UserService/UserService.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import API_KEY from "../../settings/apiKey";
-import BASE_API_URL from "../../settings/baseApiUrl";
 
 const UserService = {
   async retrieveUsers(
@@ -11,8 +10,7 @@ const UserService = {
     search = ""
   ) {
     try {
-      // TODO: Mock API
-      const response = await axios.get("/api/users", {
+      const response = await axios.get("api/users", {
         params: {
           sort: sort,
           direction: direction,
@@ -20,25 +18,12 @@ const UserService = {
           limit: limit,
           search: search,
         },
+        headers: {
+          "x-api-key": API_KEY,
+        },
       });
 
       return response.data;
-
-      // TODO: Live API
-      // const response = await axios.get(`${BASE_API_URL}/users`, {
-      //   params: {
-      //     sort: sort,
-      //     direction: direction,
-      //     offset: offset,
-      //     limit: limit,
-      //     search: search,
-      //   },
-      //   headers: {
-      //     "x-api-key": API_KEY,
-      //   },
-      // });
-
-      // return response.data;
     } catch (error) {
       console.error(error);
 
@@ -47,20 +32,14 @@ const UserService = {
   },
   async createUser(values) {
     try {
-      // TODO: Mock API
-      const response = await axios.post("/api/users", values);
+      const response = await axios.post("api/users", values, {
+        headers: {
+          "x-api-key": API_KEY,
+          "Content-Type": "application/json",
+        },
+      });
 
       return response.data;
-
-      // TODO: Live API
-      // const response = await axios.post(`${BASE_API_URL}/users`, values, {
-      //   headers: {
-      //     "x-api-key": API_KEY,
-      //     "Content-Type": "application/json",
-      //   },
-      // });
-
-      // return response.data;
     } catch (error) {
       console.error(error);
 
@@ -69,18 +48,12 @@ const UserService = {
   },
   async getUser(id) {
     try {
-      // TODO: Mock API
-      const response = await axios.get(`/api/users/${id}`);
-
+      const response = await axios.post(`api/users/${id}`, {
+        headers: {
+          "x-api-key": API_KEY,
+        },
+      });
       return response.data;
-
-      // TODO: Live API
-      // const response = await axios.post(`${BASE_API_URL}/users/${id}`, {
-      //   headers: {
-      //     "x-api-key": API_KEY,
-      //   },
-      // });
-      // return response.data;
     } catch (error) {
       console.error(error);
 
@@ -89,23 +62,13 @@ const UserService = {
   },
   async updateUser(id, values) {
     try {
-      // TODO: Mock API
-      const response = await axios.patch(`/api/users/${id}`, values);
+      const response = await axios.patch(`api/users/${id}`, values, {
+        headers: {
+          "x-api-key": API_KEY,
+        },
+      });
 
       return response.data;
-
-      // TODO: Live API
-      // const response = await axios.patch(
-      //   `${BASE_API_URL}/users/${id}`,
-      //   values,
-      //   {
-      //     headers: {
-      //       "x-api-key": API_KEY,
-      //     },
-      //   }
-      // );
-
-      // return response.data;
     } catch (error) {
       console.error(error);
 
@@ -114,15 +77,11 @@ const UserService = {
   },
   async deleteUser(id) {
     try {
-      // TODO: Mock API
-      await axios.delete(`/api/users/${id}`);
-
-      // TODO: Live API
-      // await axios.delete(`${BASE_API_URL}/users/${id}`, {
-      //   headers: {
-      //     "x-api-key": API_KEY,
-      //   },
-      // });
+      await axios.delete(`api/users/${id}`, {
+        headers: {
+          "x-api-key": API_KEY,
+        },
+      });
 
       return true;
     } catch (error) {
@@ -133,15 +92,11 @@ const UserService = {
   },
   async resendAuthentication(id) {
     try {
-      // TODO: Mock API
-      await axios.post(`/api/users/${id}/resend`);
-
-      // TODO: Live API
-      // await axios.post(`${BASE_API_URL}/users/${id}/resend`, {
-      //   headers: {
-      //     "x-api-key": API_KEY,
-      //   },
-      // });
+      await axios.post(`api/users/${id}/resend`, {
+        headers: {
+          "x-api-key": API_KEY,
+        },
+      });
 
       return true;
     } catch (error) {
