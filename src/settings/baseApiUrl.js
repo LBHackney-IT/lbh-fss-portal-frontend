f77758ab30ab77bf83f3e3cee71397119d0fe225
DanielGardiner <gardiner.daniel@hotmail.co.uk>
2020-12-10 10:06:30 +0000

    add production api url

--- src/settings/baseApiUrl.js
+++ src/settings/baseApiUrl.js
@@ -3,11 +3,11 @@ let BASE_API_URL = "";
 const location = window.location.origin;
 
 if (location.includes("find-support-services-admin.hackney")) {
-  BASE_API_URL = `update_once_available`;
+  BASE_API_URL = `https://b56cfvfyb8.execute-api.eu-west-2.amazonaws.com/production`;
 } else if (location.includes("find-support-services-staging-admin.hackney")) {
   BASE_API_URL = `https://fjilyjs06h.execute-api.eu-west-2.amazonaws.com/staging/api/v1`;
 } else {
   BASE_API_URL = "/api";
-}
+} 
 
 export default BASE_API_URL;
