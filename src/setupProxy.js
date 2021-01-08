16f20064cab9a99447204d315821771debf64bf9
Sam Wong <sam.wong@nudgedigital.co.uk>
2021-01-08 10:54:08 +0800

    Axios update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:9000/api",
+      target: "http://[::1]:9000/api",
       changeOrigin: true,
     })
   );
